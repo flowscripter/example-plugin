e52c1cf2d55dce0704aa3d0054ee322774eb7599
vectronic <vectronic@users.noreply.github.com>
2022-11-14 01:54:05 +0000

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as log from "https://deno.land/std@0.163.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.164.0/log/mod.ts";
 export * from "https://deno.land/x/flowscripter_dynamic_plugin_framework@v1.1.3/mod.ts";
 export * from "https://deno.land/x/flowscripter_example_plugin_api@v1.0.1/mod.ts";
